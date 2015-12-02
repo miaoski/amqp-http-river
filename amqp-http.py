422d934da65a1ea91d99c095c79376f1fd85ffe0
miaoski <lin.zhemin@gmail.com>
2015-12-03 00:48:09 +0800

    Use basic_get() and yield, so we don't need queue
    Saves memory and it's more like a stateless client.
    Somewhat slower, but fast enough for us.

--- amqp-http.py
+++ amqp-http.py
@@ -2,11 +2,7 @@
 # Hand-made AMQP to HTTP river, like rabbitmq-http
 
 import pika
-import Queue
-import thread
 from flask import Flask, Response
-
-q = Queue.Queue()
 
 QUEUE = 'queue_name'
 ROUTING_KEY = 'routing.key.#'
@@ -17,37 +13,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_queue():
-    size = q.qsize()
-    print 'Getting approximately %d records.' % size
-    return Response(producer(), mimetype='application/json')
+    return Response(link_amqp(), mimetype='application/json')
 
-
-def producer():
-    while not q.empty():
-        yield q.get() + '\n'
-
-
-def link_amqp(*args):
+def link_amqp():
     parameters = pika.URLParameters(AMQP)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE, passive=False, durable=False, exclusive=False, auto_delete=False, arguments={"x-message-ttl": 86400*1000})
     channel.queue_bind(queue=QUEUE, exchange=EXCHANGE, routing_key=ROUTING_KEY)
-    channel.basic_consume(callback, queue = QUEUE)
-    channel.start_consuming()
-
-
-def callback(ch, method, properties, body):
-    if not q.full():
-        q.put(body)
-        ch.basic_ack(delivery_tag = method.delivery_tag)
-    else:
-        print 'Queue is full!  Re-queue the message.'
-        ch.basic_nack()
-
+    i = 0
+    while True:
+        method_frame, header_frame, body = channel.basic_get(QUEUE)
+        if method_frame:
+            channel.basic_ack(method_frame.delivery_tag)
+            i += 1
+            yield body + '\n'
+        else:
+            break
+    print 'Returned %d records.' % i
+    connection.close()
 
 if __name__ == '__main__':
-    thread.start_new_thread(link_amqp, ())
     app.config['JSON_AS_ASCII'] = False     # JSON in UTF-8
     app.config['DEBUG'] = False
     app.run(host = '0.0.0.0', port = 8080, threaded=True)
